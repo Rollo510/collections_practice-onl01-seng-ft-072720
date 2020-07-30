5cca16b5419084f85e7aabf415ad60337df481d6
Kevin Britt <kbritt510@gmail.com>
2020-07-30 23:36:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,7 +24,7 @@ end
 def kesha_maker(array)
   new_array = []
   array.each do |letter|
-    new_array.push(letter.length[2] = "$")
+    new_array.push(letter.insert)
   end  
   return new_array
 end
