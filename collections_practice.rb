83e50b4e64dfd5bf9d17e6ecea34cdf1ec4a8ddb
Kevin Britt <kbritt510@gmail.com>
2020-07-30 23:19:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,8 +7,11 @@ def sort_array_desc(array)
 end  
 
 def sort_array_char_count(array)
-  new_array = []
   array.sort do |a, b|
     a.length <=> b.length
   end
+end
+
+def swap_elements(array)
+  
 end
